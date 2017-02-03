85966bce811e9a896a2e506a095a526c180b1983
Pablo <pabloklaschka@yahoo.de>
2017-02-03 19:10:39 +0100

    Added further functionality to PhysicalObject

--- src/pkutils/physics/PhysicalObject.java
+++ src/pkutils/physics/PhysicalObject.java
@@ -55,6 +55,26 @@ public class PhysicalObject {
      */
     protected Vector force;
 
+    public PhysicalObject(double mass, Vector position, Vector velocity, Vector acceleration, Vector force) {
+        this.mass = mass;
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+        this.force = force;
+    }
+
+    public PhysicalObject(double mass, Vector position) {
+        this(mass, position, new Vector(), new Vector(), new Vector());
+    }
+
+    public PhysicalObject(double mass) {
+        this(mass, new Vector());
+    }
+
+    public void addForce(Vector force) {
+        this.force.add(force);
+    }
+
     /**
      * @param t The forwarded time in seconds
      */
@@ -69,5 +89,60 @@ public class PhysicalObject {
 
         // pos = v * t
         position.add(Vector.getMultipliedVector(velocity, t));
+
+        // Reset force and acceleration
+        this.force = new Vector();
+        this.acceleration = new Vector();
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public Vector getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector position) {
+        this.position = position;
+    }
+
+    public Vector getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(Vector acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public Vector getForce() {
+        return force;
+    }
+
+    public void setForce(Vector force) {
+        this.force = force;
+    }
+
+    @Override
+    public String toString() {
+        return "PhysicalObject{" +
+                "mass=" + mass +
+                ", position=" + position +
+                ", velocity=" + velocity +
+                ", acceleration=" + acceleration +
+                ", force=" + force +
+                '}';
     }
 }
