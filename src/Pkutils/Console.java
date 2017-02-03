70f740a0cb820f8c62c8b80c3e72f3a0bf4c29c2
Pablo <pabloklaschka@yahoo.de>
2017-02-03 14:06:09 +0100

    Modified Pkutils.Console

--- src/Pkutils/Console.java
+++ src/Pkutils/Console.java
@@ -1,7 +1,7 @@
 package Pkutils;
 
 public class Console {
-    private static int indentionLevel;
+    private static int indentionLevel = 0;
 
     /**
      * Prints out Object in the console.
@@ -19,5 +19,24 @@ public class Console {
      */
     public static void println(Object object) {
         System.out.println(object);
+    }
+
+    /**
+     * @param object The Object that's to be printed
+     */
+    public static void printlnIndented(Object object) {
+        System.out.print("\n");
+        for (int i = 0; i <= indentionLevel; i++) {
+            System.out.print("\t");
+        }
+        System.out.print(object);
+    }
+
+    public static int getIndentionLevel() {
+        return indentionLevel;
+    }
+
+    public static void setIndentionLevel(int indentionLevel) {
+        Console.indentionLevel = indentionLevel;
     }
 }
